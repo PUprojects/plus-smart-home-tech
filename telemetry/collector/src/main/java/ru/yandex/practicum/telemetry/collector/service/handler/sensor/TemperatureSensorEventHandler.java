b0cafa21a3253be856a24a20310ed523eb91842c
Pavel Usoltsev <usoltsevdisk@yandex.ru>
2025-06-05 23:50:54 +0300

    Удалён collector на REST Добавлено начало coolector на grpc

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/sensor/TemperatureSensorEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/handler/sensor/TemperatureSensorEventHandler.java
@@ -1,29 +1,18 @@
 package ru.yandex.practicum.telemetry.collector.service.handler.sensor;
 
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.kafka.telemetry.event.TemperatureSensorAvro;
-import ru.yandex.practicum.telemetry.collector.model.events.sensor.SensorEvent;
-import ru.yandex.practicum.telemetry.collector.model.events.sensor.SensorEventType;
-import ru.yandex.practicum.telemetry.collector.model.events.sensor.TemperatureSensorEvent;
-import ru.yandex.practicum.telemetry.collector.service.KafkaEventProducer;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import ru.yandex.practicum.telemetry.collector.service.handler.SensorEventHandler;
 
 @Component
-public class TemperatureSensorEventHandler extends BaseSensorEventHandler<TemperatureSensorAvro> {
-    public TemperatureSensorEventHandler(KafkaEventProducer producer) {
-        super(producer);
+public class TemperatureSensorEventHandler implements SensorEventHandler {
+    @Override
+    public SensorEventProto.PayloadCase getPayload() {
+        return SensorEventProto.PayloadCase.TEMPERATURE_SENSOR_EVENT;
     }
 
     @Override
-    protected TemperatureSensorAvro mapToAvro(SensorEvent event) {
-        TemperatureSensorEvent temperatureSensorEvent = (TemperatureSensorEvent) event;
-        return TemperatureSensorAvro.newBuilder()
-                .setTemperatureC(temperatureSensorEvent.getTemperatureC())
-                .setTemperatureF(temperatureSensorEvent.getTemperatureF())
-                .build();
-    }
+    public void handle(SensorEventProto event) {
 
-    @Override
-    public SensorEventType getMessageType() {
-        return SensorEventType.TEMPERATURE_SENSOR_EVENT;
     }
 }
